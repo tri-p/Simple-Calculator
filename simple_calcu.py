43d5e78f5e5653fd94766d77804c85012d0d2fe1
Katrina Pabuna <kinapabuna@gmail.com>
2023-05-12 08:15:08 +0800

    Create a window for the calculator

---
+++ simple_calcu.py
@@ -0,0 +1,24 @@
+# PABUNA, KATRINA B.
+
+# Import tkinter GUI
+from tkinter import *
+
+# Define operation buttons
+
+# Create the window for choosing an operation
+calcu = Tk()
+calcu.title("Simple Calculator - 2023")
+calcu.geometry("400x200")
+
+# Display the operation buttons
+
+# Ask user to input two numbers
+
+# Evaluate the equation
+
+# Create a popup window for another calculation
+
+# Create a popup window to exit the app
+
+# ===== start =====
+calcu.mainloop()
